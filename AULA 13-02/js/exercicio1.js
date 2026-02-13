2d6fb39fbf335c0b1c0a611ef00cbddf0dda0b97
Rafael Lourenço <rafael.l.silva31@aluno.senai.br>
2026-02-13 09:42:28 -0300

    oi

--- AULA 13-02/js/exercicio1.js
+++ AULA 13-02/js/exercicio1.js
@@ -1,15 +1,19 @@
-const tituloH1 = document.querySelector('.titulo-pagina');
+const titulo = document.querySelector('.titulo');
 const container = document.querySelector('#container');
-const areaImagem = document.querySelector('#area-imagem');
+const botao = document.querySelector('.botao');
 
-tituloH1.innerText = 'Dom feito com sucesso!!!';
 
+titulo.textContent = 'Sexta feira de Carnaval!';
+
+container.style.backgroundColor = '#56e419'
 container.style.width = '94%';
 container.style.margin = '0 auto';
-container.style.backgroundColor = '#f0f0f0';
-container.style.textAlign = 'center';
-
-container.innerHTML = '<h2 id="SENAI"></h2>';
-document.querySelector('#SENAI').innerText = 'Dom, meu melhor amigo kkkkkkkkkkkkkkkkkkkkkkk';
-
-areaImagem.innerHTML = '<img src="https://hojeediadefilosofia.wordpress.com/wp-content/uploads/2017/01/bob-esponja-principal.jpg?w=1920&h=1080&crop=1" alt="Imagem">';
+let subtitulo = '<h2>Programação Front-End</h2>';
+container.innerHTML += subtitulo;
+let imagem = '<img src="./img/cidade-tech.jpg" alt="Imagem da cidade">';
+container.innerHTML += imagem;
+let contador = 0;
+botao.addEventListener('click',() => {
+    alert('Você clicou no botão ' + contador + 'vezes')
+    contador ++;
+})
